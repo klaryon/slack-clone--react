1402bd6f3d0f1f3956ad8a50c004ad0cf4e21302
klaryon <klary.ardon@gmail.com>
2021-03-20 21:30:32 +0100

    initial setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBNghXyhb3E7fOHSyYVgNyF_T544DQvgZA",
+    authDomain: "slack-clone-react-2d336.firebaseapp.com",
+    projectId: "slack-clone-react-2d336",
+    storageBucket: "slack-clone-react-2d336.appspot.com",
+    messagingSenderId: "622379045733",
+    appId: "1:622379045733:web:5c6db44ff10c5ea9ffc78c"
+  };
