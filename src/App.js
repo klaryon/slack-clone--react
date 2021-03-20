cfe14fe3b7dd6f2a4222142d7b82ca448628468c
klaryon <klary.ardon@gmail.com>
2021-03-20 21:37:52 +0100

    react-router-dom setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's build slack!</h1>
+      <Router>
+        <>
+          <Switch>
+            <Route path="/" exact>
+              <h1>This is the homepage</h1>
+            </Route>
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
