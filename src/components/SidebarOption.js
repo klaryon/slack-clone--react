5562c79265630ab7aac5a2fc93e2593b9fc5dab2
klaryon <klary.ardon@gmail.com>
2021-03-24 17:19:27 +0100

    firebase hooks

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useCollection } from "react-firebase-hooks/firestore"
+import { db } from '../firebase';
 
 function SidebarOption({ Icon, title, addChannelOption }) {
+    const [channels, loading, error] = useCollection(db.collection('rooms'));
+
+    const addChannel = () => {
+        const channelName = prompt('Please enter the channel name');
+
+        if(channelName) {
+            db.collection('rooms').add({
+                name: channelName,
+            })
+        }
+    };
+
+    const selectChannel = () => {};
+    
     return (
-        <SidebarOptionContainer>
+        <SidebarOptionContainer
+            onClick={addChannelOption ? addChannel : selectChannel}
+        >
             {Icon && <Icon fontSize="small" style={{padding: 10}}/>}
             {Icon ? <h3>{title}</h3> : (<SidebarOptionChannel>
                 <span>#</span> {title}
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyBNghXyhb3E7fOHSyYVgNyF_T544DQvgZA",
     authDomain: "slack-clone-react-2d336.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "622379045733",
     appId: "1:622379045733:web:5c6db44ff10c5ea9ffc78c"
   };
+  
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider, db };
