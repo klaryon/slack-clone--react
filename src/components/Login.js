68f38d9e5ce8f190ee278a4db02eb898f0dfd1c6
klaryon <klary.ardon@gmail.com>
2021-03-26 15:18:58 +0100

    google sign in

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import { Button } from "@material-ui/core";
+import { auth, provider } from "../firebase";
 
 function Login() {
   const signIn = (e) => {
     e.preventDefault();
+    auth.signInWithPopup(provider).catch((error) => alert(error.message));
   };
 
   return (
